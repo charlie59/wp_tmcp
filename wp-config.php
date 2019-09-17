797984b5b74ca8a402e0e37b583047486785e31d
Webdev <webdev@tmcp.com>
2019-09-17 14:12:55 +0000

    here ishoutson

--- wp-config.php
+++ wp-config.php
@@ -31,12 +31,12 @@ define('WP_DEBUG', FALSE);
 
 # WP Engine Settings
 
-#define('MULTISITE', true);
-#define('SUBDOMAIN_INSTALL', true);
-#define('DOMAIN_CURRENT_SITE', 'www.hereishouston.com');
-#define('PATH_CURRENT_SITE', '/');
-#define('SITE_ID_CURRENT_SITE', 1);
-#define('BLOG_ID_CURRENT_SITE', 1);
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', true);
+define('DOMAIN_CURRENT_SITE', 'www.hereishouston.com');
+define('PATH_CURRENT_SITE', '/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
 
 define('ADMIN_COOKIE_PATH', '/');
 define('COOKIE_DOMAIN', '');
